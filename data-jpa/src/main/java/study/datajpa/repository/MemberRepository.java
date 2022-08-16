2b8b592784ea03d55f133b9b187542e10066f45c
jujuwon <webmon@kakao.com>
2022-08-17 08:01:22 +0900

    feat: [DATA-JPA] 순수 JPA 와 Data JPA 페이징과 정렬

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package study.datajpa.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,5 +34,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	List<Member> findListByUsername(String username); // 컬렉션
 	Member findMemberByUsername(String username); // 단건
 	Optional<Member> findOptionalByUsername(String username); // 단건 Optional
+
+
+	@Query(value = "select m from Member m left join m.team", countQuery = "select count(m.username) from Member m")
+	Page<Member> findByAge(int age, Pageable pageable);
 }
 
