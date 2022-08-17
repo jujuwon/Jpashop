fda49b600548ad855ee589913af5b53aa7c63db6
jujuwon <webmon@kakao.com>
2022-08-17 10:44:25 +0900

    feat: [DATA-JPA] JPA Lock 어노테이션 추가

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,12 +3,14 @@ package study.datajpa.repository;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.LockModeType;
 import javax.persistence.QueryHint;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
@@ -57,5 +59,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	@QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
 	Member findReadOnlyByUsername(String username);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	List<Member> findLockByUsername(String username);
 }
 
