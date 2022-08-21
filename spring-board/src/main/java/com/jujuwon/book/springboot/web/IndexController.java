7af36c960f222dc52cc6c889b38ecfb8978de22a
jujuwon <webmon@kakao.com>
2022-08-21 11:28:03 +0900

    refactor: [BOARD] 세션 정보 가져오기 어노테이션으로 변경

--- spring-board/src/main/java/com/jujuwon/book/springboot/web/IndexController.java
+++ spring-board/src/main/java/com/jujuwon/book/springboot/web/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.jujuwon.book.springboot.config.auth.LoginUser;
 import com.jujuwon.book.springboot.config.auth.dto.SessionUser;
 import com.jujuwon.book.springboot.domain.posts.PostsService;
 import com.jujuwon.book.springboot.web.dto.PostsResponseDto;
@@ -21,10 +22,8 @@ public class IndexController {
 	private final HttpSession httpSession;
 
 	@GetMapping("/")
-	public String index(Model model) {
+	public String index(Model model, @LoginUser SessionUser user) {	//세션 정보 가져오기
 		model.addAttribute("posts", postsService.findAllDesc());
-
-		SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
 		if (user != null) {
 			model.addAttribute("userName", user.getName());
