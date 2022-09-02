ffbefd9125df710eec413648163fbba841f54644
jujuwon <webmon@kakao.com>
2022-09-02 10:33:29 +0900

    feat: [JWT] SecurityConfig 생성

---
+++ jwt-tutorial/src/main/java/me/jujuwon/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package me.jujuwon.jwttutorial.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity	//기본적인 Web 보안 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()	//HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다
+			.antMatchers("/api/hello").permitAll()	// /api/hello에 대한 요청은 인증없이 접근을 허용하겠다
+			.anyRequest().authenticated();	//나머지 요청들은 전부 인증을 받아야한다.
+	}
+}
