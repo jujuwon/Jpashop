8521571cfddbb798eb1cc609b54d6ef52e121dba
jujuwon <webmon@kakao.com>
2022-09-12 16:31:00 +0900

    feat: [JWT] 회원가입 및 권한검증 구현

---
+++ jwt-tutorial/src/main/java/me/jujuwon/jwttutorial/contoller/UserController.java
@@ -0,0 +1,46 @@
+package me.jujuwon.jwttutorial.contoller;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.jujuwon.jwttutorial.dto.UserDto;
+import me.jujuwon.jwttutorial.entity.User;
+import me.jujuwon.jwttutorial.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<User> signup(
+		@Valid @RequestBody UserDto userDto
+	) {
+		return ResponseEntity.ok(userService.signup(userDto));
+	}
+
+	@GetMapping("/user")
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+	public ResponseEntity<User> getMyUserInfo() {
+		return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+	}
+
+	@GetMapping("/user/{username}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<User> getUserInfo(@PathVariable String username) {
+		return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
+	}
+}
