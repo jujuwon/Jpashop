8ebb761f522ed92bb290cef830915e8c9c60a843
jujuwon <webmon@kakao.com>
2022-09-12 14:55:01 +0900

    feat: [JWT] Jwt 코드 및 SecurityConfig 등록

---
+++ jwt-tutorial/src/main/java/me/jujuwon/jwttutorial/jwt/JwtFilter.java
@@ -0,0 +1,59 @@
+package me.jujuwon.jwttutorial.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+public class JwtFilter extends GenericFilterBean {
+
+	private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
+	private TokenProvider tokenProvider;
+
+	public JwtFilter(TokenProvider tokenProvider) {
+		this.tokenProvider = tokenProvider;
+	}
+
+	// 실제 필터링 로직은 doFilter 내부에 작성
+	// doFilter 의 역할 : 토큰의 인증정보를 SecurityContext 에 저장하는 역할 수행
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+		throws IOException, ServletException {
+
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		String jwt = resolveToken(httpServletRequest);
+		String requestURI = httpServletRequest.getRequestURI();
+
+		if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+			Authentication authentication = tokenProvider.getAuthentication(jwt);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+		} else {
+			logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+		}
+
+		chain.doFilter(request, response);
+	}
+
+	// Request Header 에서 토큰 정보를 꺼내오는 메소드
+	private String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		return null;
+	}
+}
